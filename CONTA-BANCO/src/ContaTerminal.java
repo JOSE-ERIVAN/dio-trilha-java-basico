796c6a4fc8e7a30c5f899fd606964aec681e62d7
JOSE ERIVAN <j.erivan.pereira@gmail.com>
2024-09-20 21:41:06 -0300

    confirme

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o numero da agencia");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o numero da conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o saldo");
+        double saldo = scanner.nextDouble();
+
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque");
+
+
+
+        
     }
 }
